b8f2c7fec55b7f61afa8062a41c0e19eca5c69b4
Luan Pham <luanpm88@gmail.com>
2018-01-13 10:52:26 +0700

    fix model

--- app/decorators/models/order_decorator.rb
+++ app/decorators/models/order_decorator.rb
@@ -3,4 +3,9 @@ Erp::Orders::Order.class_eval do
     def self.stock_check_orders_count
         self.stock_check_orders.count
     end
+
+    # get waiting checking order count
+    def self.stock_checking_orders_count
+        self.stock_checking_orders.count
+    end
 end
