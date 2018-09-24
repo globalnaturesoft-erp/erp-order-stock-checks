2f5d24f2a0c081cabf15b70b5d85250f489fbca5
Ton Hung Nguyen <tonhungstar@gmail.com>
2018-09-24 09:28:52 +0000

    update user permission

--- app/decorators/models/ability_decorator.rb
+++ app/decorators/models/ability_decorator.rb
@@ -4,18 +4,21 @@ Erp::Ability.class_eval do
     # Create for stock check (if stock check not exist)
     can :stock_check, Erp::Orders::Order do |order|
       if order.is_stock_checking?
-        order.schecks.empty? or (!order.schecks.empty? and order.get_order_stock_check.is_done?)
+        (order.schecks.empty? or (!order.schecks.empty? and order.get_order_stock_check.is_done?)) and
+        user.get_permission(:inventory, :order_stock_checks, :schecks, :check) == 'yes'
       end
     end
     
     # Edit for stock check exist
     can :stock_check_exist, Erp::Orders::Order do |order|
-      !order.schecks.empty? and order.get_order_stock_check.is_draft?
+      (!order.schecks.empty? and order.get_order_stock_check.is_draft?) and
+      user.get_permission(:inventory, :order_stock_checks, :schecks, :check) == 'yes'
     end
     
     # Set status like done for stock check
     can :confirm_stock_check, Erp::Orders::Order do |order|
-      !order.schecks.empty? and order.get_order_stock_check.is_draft?
+      (!order.schecks.empty? and order.get_order_stock_check.is_draft?) and
+      user.get_permission(:inventory, :order_stock_checks, :schecks, :approve_order) == 'yes'
     end
   end
 end
